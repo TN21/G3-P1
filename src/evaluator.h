d240edf8c12a69c104e04e4499f008cdfeb9ea41
NathanWoodard <43422914+NathanWoodard@users.noreply.github.com>
2018-09-29 20:06:02 -0500

    Update evaluator.h

--- src/evaluator.h
+++ src/evaluator.h
@@ -3,10 +3,12 @@
 #include <string>
 using std::string;
 class Evaluator {
+    private:
+	    bool isOperator(char c);
+        bool precedenceCompare(string op1, string op2);
+        string addSpaces(string expresion);
     public:
         Evaluator();
         int evaluate(string & rawExpression);
-        string addSpaces(string expresion);
-        bool precedenceCompare(string op1, string op2);
 };
 #endif
