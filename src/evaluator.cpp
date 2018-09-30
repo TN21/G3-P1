8b6bfb5c6e2ee78b8333a43f40ed09f2b9f7e792
NathanWoodard <43422914+NathanWoodard@users.noreply.github.com>
2018-09-29 19:46:00 -0500

    Update evaluator.cpp

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -10,3 +10,65 @@ Evaluator::Evaluator() {
 int Evaluator::evaluate(string & rawExpression) {
     
 }
+string Evaluator::addSpaces(string expression)
+{
+	char current_char;
+	string spaced_expression = "";
+	string previous_type = "None";
+	for (int i = 0;i < expression.length();++i)
+	{
+		current_char = expression[i];
+		if (isdigit(current_char))
+		{
+			while (isdigit(current_char))
+			{
+				spaced_expression += current_char;
+				++i;
+				current_char = expression[i];
+			}
+			previous_type = "Number";
+			spaced_expression += " ";
+		}
+		if (isOperator(current_char))
+		{
+			spaced_expression += current_char;
+			if (current_char == expression[i + 1] && (current_char == '&' || current_char == '|'))
+			{
+				spaced_expression += expression[i + 1];
+				++i;
+			}
+			if (previous_type != "Number")
+			{
+				if (current_char == expression[i + 1] && (current_char == '+' || current_char == '-'))
+				{
+					spaced_expression += expression[i + 1];
+					++i;
+				}
+				if (current_char == '-' && isdigit(expression[i + 1]))
+				{
+					++i;
+					current_char = expression[i];
+					while (isdigit(current_char))
+					{
+						spaced_expression += current_char;
+						++i;
+						current_char = expression[i];
+					}
+					--i;
+				}
+			}
+			if ((current_char == '>' || current_char == '<' || current_char == '=' || current_char == '!') && expression[i + 1] == '=')
+			{
+				spaced_expression += expression[i + 1];
+				++i;
+
+			}
+			if (isdigit(spaced_expression[spaced_expression.length() - 1]))
+				previous_type = "Number";
+			else
+				previous_type = "Operator";
+		}
+		spaced_expression += " ";
+	}
+	return spaced_expression;
+}
